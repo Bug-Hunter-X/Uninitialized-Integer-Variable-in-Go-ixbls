68b7ae8c15a3955bf4f2742f7c53a9bba5781de8
BugHunter-X <askzenai@gmail.com>
2025-02-01 01:22:04 +0100

    Add bugSolution.go file

---
+++ bugSolution.go
@@ -0,0 +1,12 @@
+func main() {
+    var i int = 0 // Explicit initialization to 0
+    fmt.Println(i)
+    i++
+    fmt.Println(i)
+    
+    //Alternative Initialization using := operator
+    j := 0 // shorter and better solution
+    fmt.Println(j)
+    j++
+    fmt.Println(j)
+}
